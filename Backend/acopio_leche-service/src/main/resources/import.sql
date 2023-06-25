9dd6047c5f75825b041773da923137726abcfe31
Ivan <ivan.zuniga@usach.cl>
2023-06-25 15:21:06 -0400

    [Frontend] Se crean las vistas del Fileinformation  del acopio y valores

---
+++ Backend/acopio_leche-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO datos_acopio(fecha,turno,proveedor,kls_leche) VALUES ("2023/06/12","M", "01111", 70.0);
+INSERT INTO datos_acopio(fecha,turno,proveedor,kls_leche) VALUES ("2023/06/13","M", "01112", 55.0);
+INSERT INTO datos_acopio(fecha,turno,proveedor,kls_leche) VALUES ("2023/06/14","T", "01123", 80.0);
+INSERT INTO datos_acopio(fecha,turno,proveedor,kls_leche) VALUES ("2023/06/15","T", "01234", 40.0);
---
+++ Backend/valores_gs-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO datos_gs(proveedor,grasa,solido) VALUES ("01111", 13.0,16.0);
+INSERT INTO datos_gs(proveedor,grasa,solido) VALUES ("01112", 15.0,15.0);
+INSERT INTO datos_gs(proveedor,grasa,solido) VALUES ("01123", 8.0,6.0);
+INSERT INTO datos_gs(proveedor,grasa,solido) VALUES ("01234", 10.0,11.0);
